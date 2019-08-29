5fdd99259d9b66b35a281dbbe911cfa35221e1ef
Mary Farner <maryfarner@gmail.com>
2019-08-29 00:57:10 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -28,6 +28,7 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   answer = gets.strip
+  index = answer.to_i
   if answer.to_i > 0 && answer.to_i <= songs.length then
     puts "Playing #{songs[answer.to_i + 1]}"
   elsif songs.any?(answer) then
