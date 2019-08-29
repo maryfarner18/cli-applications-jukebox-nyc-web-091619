ae0d884a28c53ea4dfa0666bc43486a684248bde
Mary Farner <maryfarner@gmail.com>
2019-08-29 00:56:00 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -14,7 +14,9 @@ def help
   puts "I accept the following commands:"
   puts "- help : displays this help message"
   puts "- list : displays a list of songs you can play" 
+  puts "- play : lets you choose a song to play"
   puts "- exit : exits this program"
+  
 end
 
 def list(songs)
